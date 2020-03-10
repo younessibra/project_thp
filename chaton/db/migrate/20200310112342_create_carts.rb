c5a2a1ac64f7631a146017d55526619bc5e245ff
younessibra <youness.ibra@gmail.com>
2020-03-10 13:14:18 +0100

    create cart table

---
+++ chaton/db/migrate/20200310112342_create_carts.rb
@@ -0,0 +1,8 @@
+class CreateCarts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :carts do |t|
+      t.belongs_to :item, index: true
+      t.belongs_to :user, index: true
+    end
+  end
+end
